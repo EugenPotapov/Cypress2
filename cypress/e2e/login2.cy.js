372cd2c69cca871baf824492a895f78fd0d238af
EugenPotapov <132608815+EugenPotapov@users.noreply.github.com>
2024-03-03 18:01:58 +0300

    code changed

--- cypress/e2e/login2.cy.js
+++ cypress/e2e/login2.cy.js
@@ -1,3 +1,9 @@
+const book = {
+  title: "Лабиринт отражений",
+  description: "Киберпанк",
+  author: "Сергей Лукьяненко",
+};
+
 describe("login page", () => {
   beforeEach(() => {
     cy.visit("/");
@@ -5,28 +11,25 @@ describe("login page", () => {
   });
 
   it("login succesfully", () => {
-    cy.viewport(1920, 1080);
     cy.contains("Добро пожаловать bropet@mail.ru").should("be.visible");
-    cy.contains("Log out").should("be.visible");
   });
 
   it("Added book", () => {
-    cy.viewport("iphone-x");
-    cy.contains("Add new").click();
-    cy.contains("Book description").should("be.visible");
-    cy.get("#title").type("Лабиринт отражений");
-    cy.get("#description").type("Киберпанк");
-    cy.get("#authors").type("Сергей Лукьяненко");
-    cy.get("#favorite").click();
-    cy.contains("Submit").click();
+    cy.addBook(book);
+    cy.contains(book.title).should("be.visible");
   });
 
   it("Add to favorite", () => {
-    cy.viewport("macbook-15");
-    cy.contains("Add to favorite").click();
+    cy.addFavorite(book);
+    cy.visit("/favorites");
+    cy.get(".card-title").should("contain.text", book.title);
   });
 
   it("Delete book from favorite", () => {
-    cy.contains("Delete from favorite").click();
+    cy.visit("/favorites");
+    cy.contains(book.title)
+      .should("be.visible")
+      .within(() => cy.get(".card-footer > .btn").click({ force: true }));
+    cy.contains(book.title).should("not.exist");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,10 +23,38 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
 
 Cypress.Commands.add("login", (email, password) => {
   cy.contains("Log in").click();
   if (email) cy.get("#mail").type(email);
   if (password) cy.get("#pass").type(password);
+  cy.contains("Submit").click();
+});
+
+Cypress.Commands.add("addBook", (book) => {
+  cy.contains("Add new").click();
+  cy.contains("Book description");
+  cy.get("#title").type(book.title);
+  cy.get("#description").type(book.description);
+  cy.get("#authors").type(book.author);
+  cy.contains("Submit").click();
+});
+
+Cypress.Commands.add("addFavorite", (book) => {
+  cy.contains("Add new").click();
+  cy.contains("Book description");
+  cy.get("#title").type(book.title);
+  cy.get("#description").type(book.description);
+  cy.get("#authors").type(book.author);
+  cy.get("#favorite").click();
+  cy.contains("Submit").click();
+});
+
+Cypress.Commands.add("delFavorite", (book) => {
+  cy.contains("Add new").click();
+  cy.get("input#title").type(book.title);
+  cy.get("input#description").type(book.description);
+  cy.get("input#authors").type(book.author);
   cy.contains("Submit").click();
 });
